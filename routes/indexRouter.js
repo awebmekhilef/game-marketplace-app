e5d96e506ed2b5289c37cf2d1c25dcced314f8bb
awebmekhilef <awebmekhilef@gmail.com>
2020-12-08 10:40:08 +0800

    View all games

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,17 @@
 const router = require('express').Router()
 
+const Game = require('../models/game')
+
 router.get('/', async (req, res) => {
-	res.render('home')
+	try {
+		const games = await Game.find({})
+
+		res.render('home', {
+			games
+		})
+	} catch (err) {
+		res.redirect('/')
+	}
 })
 
 module.exports = router
