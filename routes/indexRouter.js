557fa993d59764151a173453adfe01cc0afd4518
awebmekhilef <awebmekhilef@gmail.com>
2020-12-14 11:17:48 +0800

    Use genre name instead of id in query

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,11 +6,12 @@ const Genre = require('../models/genre')
 router.get('/', async (req, res) => {
 	try {
 		let query = Game.find()
+		const genre = await Genre.findOne({ name: req.query.genre })
 
 		if (req.query.title != null && req.query.title.trim() !== '')
 			query = query.regex('title', new RegExp(req.query.title, 'i'))
 		if (req.query.genre != null && req.query.genre !== '')
-			query = query.where({ genre: req.query.genre })
+			query = query.where({ genre: genre.id })
 
 		const games = await query.exec()
 		const genres = await Genre.find({})
